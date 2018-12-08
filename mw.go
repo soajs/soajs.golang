827009341150a63015b0ae7fd085e60229f5e63b
antoinehage <me@antoinehage.com>
2018-12-08 15:01:08 -0500

    added register if manual deploy

--- mw.go
+++ mw.go
@@ -1,128 +1,157 @@
 package soajsGo
 
 import (
-    "context"
-    "encoding/json"
-    "errors"
-    "log"
-    "net/http"
-    "os"
-    "reflect"
-    "strconv"
-    "strings"
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 type SOAJSObject struct {
-  Tenant                    Tenant                    `json:"tenant"`
-  Urac                      Urac                      `json:"urac"`
-  ServicesConfig            map[string]interface{}    `json:"servicesConfig"`
-  Device                    string                    `json:"device"`
-  Geo                       map[string]string         `json:"geo"`
-  Awareness                 Awareness                 `json:"awareness"`
-  Controller                string                    `json:"controller"`
-  Reg                       RegistryObj               `json:"reg"`
+	Tenant         Tenant                 `json:"tenant"`
+	Urac           Urac                   `json:"urac"`
+	ServicesConfig map[string]interface{} `json:"servicesConfig"`
+	Device         string                 `json:"device"`
+	Geo            map[string]string      `json:"geo"`
+	Awareness      Awareness              `json:"awareness"`
+	Controller     string                 `json:"controller"`
+	Reg            RegistryObj            `json:"reg"`
 }
 
 type JSON = map[string]interface{}
+
 var globalConfig JSON
 
 func mapInjectedObject(r *http.Request) (SOAJSData, error) {
-    soajsHeader := r.Header.Get("soajsinjectobj")
+	soajsHeader := r.Header.Get("soajsinjectobj")
 
-    var input, output SOAJSData
-    if inputType := reflect.TypeOf(soajsHeader).String(); inputType == "string" {
-        if jsonError := json.Unmarshal([]byte(soajsHeader), &input); jsonError != nil {
-            return SOAJSData{}, errors.New("Unable to parse SOAJS header object")
-        }
-    }
+	var input, output SOAJSData
+	if inputType := reflect.TypeOf(soajsHeader).String(); inputType == "string" {
+		if jsonError := json.Unmarshal([]byte(soajsHeader), &input); jsonError != nil {
+			return SOAJSData{}, errors.New("unable to parse SOAJS header object")
+		}
+	}
 
-    // map information to output
-    output.Tenant = input.Tenant
-    output.Key = input.Key
-    output.Application = input.Application
-    output.Package = input.Package
-    output.Device = input.Device
-    output.Geo = input.Geo
-    output.Urac = input.Urac
-    output.Awareness = input.Awareness
+	// map information to output
+	output.Tenant = input.Tenant
+	output.Key = input.Key
+	output.Application = input.Application
+	output.Package = input.Package
+	output.Device = input.Device
+	output.Geo = input.Geo
+	output.Urac = input.Urac
+	output.Awareness = input.Awareness
 
-    return output, nil
+	return output, nil
 }
 
 func (a Awareness) GetHost(args ...string) string {
-    var serviceName, version string
-    switch len(args) {
-        //controller
-    case 1:
-        serviceName = args[0]
-        //controller, 1
-    case 2:
-        serviceName = args[0]
-        version = args[1]
-        //controller, 1, dash [dash is ignored]
-    case 3:
-        serviceName = args[0]
-        version = args[1]
-    }
+	var serviceName, version string
+	switch len(args) {
+	//controller
+	case 1:
+		serviceName = args[0]
+		//controller, 1
+	case 2:
+		serviceName = args[0]
+		version = args[1]
+		//controller, 1, dash [dash is ignored]
+	case 3:
+		serviceName = args[0]
+		version = args[1]
+	}
 
-    host := a.Host
+	host := a.Host
 
-    if serviceName != "" && strings.ToLower(serviceName) != "controller" {
-        host += ":" + strconv.Itoa(a.Port) + "/"
-        host += serviceName + "/"
+	if serviceName != "" && strings.ToLower(serviceName) != "controller" {
+		host += ":" + strconv.Itoa(a.Port) + "/"
+		host += serviceName + "/"
 
-        if _, err := strconv.Atoi(version); err == nil {
-            host += "v" + version + "/"
-        }
-    }
+		if _, err := strconv.Atoi(version); err == nil {
+			host += "v" + version + "/"
+		}
+	}
 
-    return host
+	return host
 }
 
 func SoajsMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        injectObject, err := mapInjectedObject(r)
-        if err != nil {
-            log.Println(err)
-        } else {
-            middlewareOutput := SOAJSObject{}
-            middlewareOutput.Tenant = injectObject.Tenant
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		injectObject, err := mapInjectedObject(r)
+		if err != nil {
+			log.Println(err)
+		} else {
+			middlewareOutput := SOAJSObject{}
+			middlewareOutput.Tenant = injectObject.Tenant
 
-            middlewareOutput.Tenant.Key.IKey = injectObject.Key.IKey
-            middlewareOutput.Tenant.Key.EKey = injectObject.Key.EKey
+			middlewareOutput.Tenant.Key.IKey = injectObject.Key.IKey
+			middlewareOutput.Tenant.Key.EKey = injectObject.Key.EKey
 
-            middlewareOutput.Tenant.Application = injectObject.Application
-            middlewareOutput.Tenant.Application.Package_acl = injectObject.Package.Acl
-            middlewareOutput.Tenant.Application.Package_acl_all_env = injectObject.Package.Acl_all_env
+			middlewareOutput.Tenant.Application = injectObject.Application
+			middlewareOutput.Tenant.Application.Package_acl = injectObject.Package.Acl
+			middlewareOutput.Tenant.Application.Package_acl_all_env = injectObject.Package.Acl_all_env
 
-            middlewareOutput.Urac = injectObject.Urac
-            middlewareOutput.ServicesConfig = injectObject.Key.Config
-            middlewareOutput.Device = injectObject.Device
-            middlewareOutput.Geo = injectObject.Geo
-            middlewareOutput.Awareness = injectObject.Awareness
+			middlewareOutput.Urac = injectObject.Urac
+			middlewareOutput.ServicesConfig = injectObject.Key.Config
+			middlewareOutput.Device = injectObject.Device
+			middlewareOutput.Geo = injectObject.Geo
+			middlewareOutput.Awareness = injectObject.Awareness
 
-            if os.Getenv("SOAJS_REGISTRY_API") != "" && os.Getenv("SOAJS_ENV") != "" {
-                middlewareOutput.Reg = regObj
-            }
+			if os.Getenv("SOAJS_REGISTRY_API") != "" && os.Getenv("SOAJS_ENV") != "" {
+				middlewareOutput.Reg = regObj
+			}
 
-            soajs := context.WithValue(r.Context(), "soajs", middlewareOutput)
-            r = r.WithContext(soajs)
-        }
-        next.ServeHTTP(w, r)
-    })
+			soajs := context.WithValue(r.Context(), "soajs", middlewareOutput)
+			r = r.WithContext(soajs)
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
-func InitMiddleware(config JSON) (func(http.Handler) http.Handler) {
-    globalConfig = config
+func InitMiddleware(config JSON) func(http.Handler) http.Handler {
+	globalConfig = config
 
-    serviceName := globalConfig["serviceName"].(string)
+	serviceName := globalConfig["serviceName"].(string)
 
-    registryApi := os.Getenv("SOAJS_REGISTRY_API")
-    soajsEnv := os.Getenv("SOAJS_ENV")
-    if soajsEnv != "" && registryApi != "" {
-        params := map[string]string{"envCode": strings.ToLower(soajsEnv), "serviceName": serviceName}
-        AutoReload(params)
-    }
+	registryApi := os.Getenv("SOAJS_REGISTRY_API")
+	soajsEnv := os.Getenv("SOAJS_ENV")
+	if soajsEnv != "" && registryApi != "" {
+		params := map[string]string{"envCode": strings.ToLower(soajsEnv), "serviceName": serviceName}
+		AutoReload(params)
 
-    return SoajsMiddleware
+		manualDeploy := os.Getenv("SOAJS_DEPLOY_MANUAL")
+		if manualDeploy == "1" {
+			mwIP := "127.0.0.1"
+			if globalConfig["IP"] != nil {
+				mwIP = globalConfig["IP"].(string)
+			}
+			reqUrl := "http://" + registryApi + "/register"
+			globalConfig["ip"] = mwIP
+			globalConfig["type"] = "service"
+			globalConfig["mw"] = "true"
+			globalConfig["group"] = globalConfig["serviceGroup"]
+			globalConfig["name"] = globalConfig["serviceName"]
+			globalConfig["port"] = globalConfig["servicePort"]
+			globalConfig["version"] = globalConfig["serviceVersion"]
+			bytesRepresentation, err := json.Marshal(globalConfig)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			httpResponse, err := http.Post(reqUrl, "application/json", bytes.NewBuffer(bytesRepresentation))
+			if err != nil {
+				log.Println("unable to get registry host @ gateway")
+				log.Println(err)
+			} else {
+				defer httpResponse.Body.Close()
+			}
+		}
+	}
+
+	return SoajsMiddleware
 }
